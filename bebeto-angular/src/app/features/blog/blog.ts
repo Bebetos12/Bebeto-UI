7ca1223fb5e4d037cf75375b578d0e4f67db3a80
Bebetos12 <mezimorel11@gmail.com>
2025-12-24 10:56:10 +0200

    update code

--- bebeto-angular/src/app/features/blog/blog.ts
+++ bebeto-angular/src/app/features/blog/blog.ts
@@ -13,21 +13,21 @@ export class BlogComponent {
     {
       image: 'Assets/for blog shapes image/Rectangle 6.png',
       category: 'UI/UX Design',
-      author: 'Bebeto',
+      author: 'Jayesh Patil', // Changed author name
       date: '10 Nov, 2023',
       title: 'Design Unraveled: Behind the Scenes of UI/UX Magic'
     },
     {
       image: 'Assets/for blog shapes image/Rectangle 6-1.png',
       category: 'App Design',
-      author: 'Bebeto',
+      author: 'Jayesh Patil', // Changed author name
       date: '09 Oct, 2023',
       title: 'Sugee: Loan Management System for Rural Sector.'
     },
     {
       image: 'Assets/for blog shapes image/Rectangle 6-2.png',
       category: 'App Design',
-      author: 'Bebeto',
+      author: 'Jayesh Patil', // Changed author name
       date: '13 Aug, 2023',
       title: 'Cinetrade: Innovative way to invest in Digital Media'
     }
